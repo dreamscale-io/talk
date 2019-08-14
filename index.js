a9d9af5bd4c63d4e5244eccc0a5e626a2a0eb38b
1-14x0r <kara@dreamscale.io>
2019-08-13 17:17:12 -0700

    reworking server code to handle various paths for namespaces

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const PORT = process.env.PORT || 3000;
 
 const server = require('http').createServer();
 
-const io = require('socket.io')(server, {
-    path: '/test',
+const memberNsp = require('socket.io')(server, {
+    path: '/member',
+    serveClient: false,
+    // below are engine.IO options
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+const teamNsp = require('socket.io')(server, {
+    path: '/team',
+    serveClient: false,
+    // below are engine.IO options
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+const organizationNsp = require('socket.io')(server, {
+    path: '/organization',
     serveClient: false,
     // below are engine.IO options
     pingInterval: 10000,
