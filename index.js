08baa149903043797ea86b0dad29d3148436c5de
1-14x0r <kara@dreamscale.io>
2019-08-13 17:32:31 -0700

    added custom path for rt-flow

--- index.js
+++ index.js
@@ -2,30 +2,8 @@ const PORT = process.env.PORT || 3000;
 
 const server = require('http').createServer();
 
-const memberNsp = require('socket.io')(server, {
-    path: '/member',
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-});
-
-memberNsp.on('connection', function(socket){
-    console.log('someone connected');
-});
-
-const teamNsp = require('socket.io')(server, {
-    path: '/team',
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-});
-
-const organizationNsp = require('socket.io')(server, {
-    path: '/organization',
+const io = require('socket.io')(server, {
+    path: '/test',
     serveClient: false,
     // below are engine.IO options
     pingInterval: 10000,
@@ -34,5 +12,3 @@ const organizationNsp = require('socket.io')(server, {
 });
 
 server.listen(PORT);
-
-console.log("server started on port " + PORT);
