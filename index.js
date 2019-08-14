b62d991aa168b2bf191f3d0d2b3e3860c18c1f2d
1-14x0r <kara@dreamscale.io>
2019-08-14 12:16:49 -0700

    cleaned up code and set default port to 5000

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const PORT = process.env.PORT || 5000;
+const INDEX = path.join(__dirname, "index.html");
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX) )
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use((req, res) => res.sendFile(INDEX))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+io.on("connection", (socket) => {
+    console.log("Client connected " + socket.id);
+    socket.on("disconnect", (socket) => {
+        console.log("Client disconnected " + socket.id)
+    });
 });
