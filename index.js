409dfcdd3b4073f1e8ca7075bed2eb315617e834
1-14x0r <kara@dreamscale.io>
2019-08-14 12:29:19 -0700

    changed port to 5050 to fix conflict

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const socketIO = require("socket.io");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 const INDEX = path.join(__dirname, "index.html");
 
 const server = express()
