5e41c36d7a220eeec316293a180c211c1310e35f
ZoeDreams <kara@dreamscale.io>
2020-01-22 15:42:44 -0600

    add app metrics

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node --require appmetrics/start index.js
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/**
+ * Application Metrics
+ */
+var appmetrics = require('appmetrics');
+var monitoring = appmetrics.monitor();
+monitoring.on('initialized', function (env) {
+  env = monitoring.getEnvironment();
+  for (var entry in env) {
+    console.log(entry + ':' + env[entry]);
+  };
+});
+
+/**
+ * DreamScale Server
+ * @type {Server}
+ */
 const Server = require("./public/Server");
 var server = new Server();
 server.start();
